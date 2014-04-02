432871d1153f63f7f1662a68784473cd7587e56b
Lars Schuetze <laze1989@gmail.com>
2014-04-02 10:26:43 +0200

    declare own strings to remove discouraged access to internal classes of GEF

--- ORM/ORMEditor/src/orm/editor/ORMGraphicalEditorActionBarContributor.java
+++ ORM/ORMEditor/src/orm/editor/ORMGraphicalEditorActionBarContributor.java
@@ -1,6 +1,5 @@
 package orm.editor;
 
-import org.eclipse.gef.internal.GEFMessages;
 import org.eclipse.gef.ui.actions.ActionBarContributor;
 import org.eclipse.gef.ui.actions.DeleteRetargetAction;
 import org.eclipse.gef.ui.actions.GEFActionConstants;
@@ -11,46 +10,51 @@ import org.eclipse.jface.action.IToolBarManager;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.actions.ActionFactory;
 import org.eclipse.ui.actions.RetargetAction;
-import org.eclipse.ui.part.MultiPageEditorPart;
 
 /**
  * @author KAy Bierzynski
+ * @author Lars Schuetze
  * */
-public class ORMGraphicalEditorActionBarContributor extends
-		ActionBarContributor {
+public class ORMGraphicalEditorActionBarContributor extends ActionBarContributor {
 
-	 @Override
-	  protected void buildActions() {
-	    addRetargetAction(new UndoRetargetAction());
-	    addRetargetAction(new RedoRetargetAction());
-	    addRetargetAction(new DeleteRetargetAction());
-	    addRetargetAction(new RetargetAction(GEFActionConstants.TOGGLE_GRID_VISIBILITY, GEFMessages.ToggleGrid_Label, IAction.AS_CHECK_BOX));  
-	    addRetargetAction(new RetargetAction(GEFActionConstants.TOGGLE_SNAP_TO_GEOMETRY, GEFMessages.ToggleSnapToGeometry_Label, IAction.AS_CHECK_BOX));
-	    
-	 }
-	 
-	 // is called from the ORMMultiPageEditor pageChange when user switch between editors
-	 @Override
-	public void setActiveEditor(IEditorPart editor) {
-		super.setActiveEditor(editor);
-	 // creats actionbar for active editor with actionregistry/graphicalViewer/CommandStack from the active editor
-		getActionBars().updateActionBars();
-	}
-	  @Override
-	  public void contributeToToolBar(IToolBarManager toolBarManager) {
-	    super.contributeToToolBar(toolBarManager);
-	    toolBarManager.add(getAction(ActionFactory.UNDO.getId()));
-	    toolBarManager.add(getAction(ActionFactory.REDO.getId()));
-	    toolBarManager.add(getAction(ActionFactory.DELETE.getId()));
-	    toolBarManager.add(getAction(GEFActionConstants.TOGGLE_GRID_VISIBILITY));
-	    toolBarManager.add(getAction(GEFActionConstants.TOGGLE_SNAP_TO_GEOMETRY));
-	  }
+  private static String TOGGLE_GRID_LABEL = "&Grid";
+  private static String TOGGLE_SNAP_TO_GERMETRY_LABEL = "Snap to Geo&metry";
 
-	@Override
-	protected void declareGlobalActionKeys() {
-		// TODO Auto-generated method stub
+  @Override
+  protected void buildActions() {
+    addRetargetAction(new UndoRetargetAction());
+    addRetargetAction(new RedoRetargetAction());
+    addRetargetAction(new DeleteRetargetAction());
+    addRetargetAction(new RetargetAction(GEFActionConstants.TOGGLE_GRID_VISIBILITY,
+        TOGGLE_GRID_LABEL, IAction.AS_CHECK_BOX));
+    addRetargetAction(new RetargetAction(GEFActionConstants.TOGGLE_SNAP_TO_GEOMETRY,
+        TOGGLE_SNAP_TO_GERMETRY_LABEL, IAction.AS_CHECK_BOX));
 
-	}
-	
-	
+  }
+
+  // is called from the ORMMultiPageEditor pageChange when user switch between editors
+  @Override
+  public void setActiveEditor(IEditorPart editor) {
+    super.setActiveEditor(editor);
+    // creats actionbar for active editor with actionregistry/graphicalViewer/CommandStack from the
+    // active editor
+    getActionBars().updateActionBars();
+  }
+
+  @Override
+  public void contributeToToolBar(IToolBarManager toolBarManager) {
+    super.contributeToToolBar(toolBarManager);
+    toolBarManager.add(getAction(ActionFactory.UNDO.getId()));
+    toolBarManager.add(getAction(ActionFactory.REDO.getId()));
+    toolBarManager.add(getAction(ActionFactory.DELETE.getId()));
+    toolBarManager.add(getAction(GEFActionConstants.TOGGLE_GRID_VISIBILITY));
+    toolBarManager.add(getAction(GEFActionConstants.TOGGLE_SNAP_TO_GEOMETRY));
+  }
+
+  @Override
+  protected void declareGlobalActionKeys() {
+    // TODO Auto-generated method stub
+  }
+
+
 }
