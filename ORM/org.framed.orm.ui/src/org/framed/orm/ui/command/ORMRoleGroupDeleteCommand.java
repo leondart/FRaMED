bd17e9a9eaefe96f208be52788e79657b306d985
Kay Bierzynski <s8348434@mail.zih.tu-dresden.de>
2014-04-25 01:25:36 +0200

    some code formatting and the fix for issue #29

--- ORM/org.framed.orm.ui/src/org/framed/orm/ui/command/ORMRoleGroupDeleteCommand.java
+++ ORM/org.framed.orm.ui/src/org/framed/orm/ui/command/ORMRoleGroupDeleteCommand.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.eclipse.gef.commands.Command;
-
+import org.framed.orm.model.AbstractRole;
 import org.framed.orm.model.Node;
 import org.framed.orm.model.Relation;
 import org.framed.orm.model.RoleGroup;
@@ -16,65 +16,104 @@ import org.framed.orm.model.Rolemodel;
  * @author Kay Bierzynski
  * */
 public class ORMRoleGroupDeleteCommand extends Command {
-	
-	
-	 private RoleGroup roleGroup,parentRoleGroup;
-	 private Rolemodel parentrolemodel;
-	 
-	  private List<Relation> relations;
-	  /** Sources for the relations that start or end at this node. */
-	  private Map<Relation, Node> sourceLinks;
-	  /** Targets for the relations that start or end at this node. */
-	  private Map<Relation, Node> targetLinks;
-	 
-	  @Override
-	  public void execute() {
-		detachLinks();
-	    roleGroup.setParentRolemodel(null);
-	    roleGroup.setParentRoleGroup(null);
-	  }
-	 
-	  @Override
-	  public void undo() {
-		reattachLinks();
-		if(parentrolemodel != null)  roleGroup.setParentRolemodel(parentrolemodel);
-		if(parentRoleGroup != null)  roleGroup.setParentRoleGroup(parentRoleGroup);
-	  }
-	 
-	  /**
-	   * Detach all links from the node and from other
-	   * connecting types, storing the connection information in local
-	   * data structures.
-	   */
-	  private void detachLinks() {
-	    relations = new ArrayList<Relation>();
-	    sourceLinks = new HashMap<Relation,Node>();
-	    targetLinks = new HashMap<Relation,Node>();
-	    relations.addAll(roleGroup.getIncomingLinks());
-	    relations.addAll(roleGroup.getOutgoingLinks());
-	    for (Relation link : relations) {
-	      sourceLinks.put(link, link.getSource());
-	      targetLinks.put(link, link.getTarget());
-	      link.setSource(null);
-	      link.setTarget(null);
-	      link.setRelationContainer(null);
-	    }
-	  }
-	  
-	  /**
-	   * Reattach all links to their source and target node.
-	   */
-	  private void reattachLinks() {
-	    for (Relation link : relations) {
-	      link.setSource(sourceLinks.get(link));
-	      link.setTarget(targetLinks.get(link));
-	     if(parentrolemodel != null) link.setRelationContainer(parentrolemodel);
-	    }
-	  }
-	  
-	  public void setRoleGroup(RoleGroup roleGroup) {
-	    this.roleGroup = roleGroup;
-	    this.parentrolemodel = roleGroup.getParentRolemodel();
-	    this.parentRoleGroup = roleGroup.getParentRoleGroup();
-	  }
+
+
+  private RoleGroup roleGroup, parentRoleGroup;
+  private Rolemodel parentrolemodel;
+
+  private List<Relation> relations;
+  /** List which contains all roletypes and rolegroups that are in this rolegroup */
+  private List<AbstractRole> items;
+  /** Sources for the relations that start or end at this node. */
+  private Map<Relation, Node> sourceLinks;
+  /** Targets for the relations that start or end at this node. */
+  private Map<Relation, Node> targetLinks;
+
+
+  @Override
+  public void execute() {
+    detachLinks();
+    roleGroup.setParentRolemodel(null);
+    roleGroup.setParentRoleGroup(null);
+  }
+
+  @Override
+  public void undo() {
+    reattachLinks();
+    if (parentrolemodel != null)
+      roleGroup.setParentRolemodel(parentrolemodel);
+    if (parentRoleGroup != null)
+      roleGroup.setParentRoleGroup(parentRoleGroup);
+  }
+
+  /**
+   * Detach all links from the node and from other connecting types, storing the connection
+   * information in local data structures.
+   */
+  private void detachLinks() {
+    relations = new ArrayList<Relation>();
+    items = new ArrayList<AbstractRole>();
+    sourceLinks = new HashMap<Relation, Node>();
+    targetLinks = new HashMap<Relation, Node>();
+
+    items.addAll(roleGroup.getItems());
+
+    gatherRoleGroupsItems(items);
+
+    for (AbstractRole item : items) {
+      // add all relations with the source not in the RG and the target in the RG
+      for (Relation rel : ((Node) item).getIncomingLinks()) {
+        if (!items.contains(rel.getSource()))
+          relations.add(rel);
+      }
+      // add all relations with the source in the RG and the target not in the RG
+      for (Relation rel : ((Node) item).getOutgoingLinks()) {
+        if (!items.contains(rel.getTarget()))
+          relations.add(rel);
+      }
+
+    }
+
+    relations.addAll(roleGroup.getIncomingLinks());
+    relations.addAll(roleGroup.getOutgoingLinks());
+
+    for (Relation link : relations) {
+      sourceLinks.put(link, link.getSource());
+      targetLinks.put(link, link.getTarget());
+      link.setSource(null);
+      link.setTarget(null);
+      link.setRelationContainer(null);
+    }
+  }
+
+  private void gatherRoleGroupsItems(List<AbstractRole> items) {
+    List<AbstractRole> items2 = new ArrayList<AbstractRole>();
+    for (AbstractRole item : items) {
+      if (item instanceof RoleGroup) {
+        items2.addAll(((RoleGroup) item).getItems());
+        gatherRoleGroupsItems(items2);
+      }
+    }
+    items.addAll(items2);
+  }
+
+  /**
+   * Reattach all links to their source and target node.
+   */
+  private void reattachLinks() {
+    for (Relation link : relations) {
+      link.setSource(sourceLinks.get(link));
+      link.setTarget(targetLinks.get(link));
+      if (parentrolemodel != null)
+        link.setRelationContainer(parentrolemodel);
+      if (parentRoleGroup != null)
+        link.setRelationContainer(parentRoleGroup);
+    }
+  }
+
+  public void setRoleGroup(RoleGroup roleGroup) {
+    this.roleGroup = roleGroup;
+    this.parentrolemodel = roleGroup.getParentRolemodel();
+    this.parentRoleGroup = roleGroup.getParentRoleGroup();
+  }
 }
