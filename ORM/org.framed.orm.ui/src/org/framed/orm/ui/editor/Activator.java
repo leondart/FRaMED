59cc809258d817b950254fca8df3f06f5885533f
Lars Schuetze <dev@larsschuetze.de>
2014-04-24 09:16:12 +0200

    fixed plugin id

--- ORM/org.framed.orm.ui/src/org/framed/orm/ui/editor/Activator.java
+++ ORM/org.framed.orm.ui/src/org/framed/orm/ui/editor/Activator.java
@@ -9,7 +9,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
   // The plug-in ID
-  public static final String PLUGIN_ID = "ORMEditor"; //$NON-NLS-1$
+  public static final String PLUGIN_ID = "org.framed.orm.ui"; //$NON-NLS-1$
 
   // The shared instance
   private static Activator plugin;
