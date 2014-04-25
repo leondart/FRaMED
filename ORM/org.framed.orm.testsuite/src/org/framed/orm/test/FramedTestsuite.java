77a2c1039aca34d4f11167b22c7474037748544d
Lars Schuetze <dev@larsschuetze.de>
2014-04-25 09:25:13 +0200

    Added initial test suite. This test suite can be run either as normal JUnit Test or as JUnit Plugin-Test. Both tests should be run.

---
+++ ORM/org.framed.orm.testsuite/src/org/framed/orm/test/FramedTestsuite.java
@@ -0,0 +1,20 @@
+package org.framed.orm.test;
+
+import org.framed.orm.model.tests.ORMAllTests;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * This class collects all test suites.
+ * 
+ * @author Lars Schuetze
+ * 
+ */
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ORMAllTests.class})
+public class FramedTestsuite {
+  /*
+   * This class remains empty
+   */
+}
