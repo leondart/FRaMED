de85fe1e2b8636cca5e4ccca2e494b508636329e
Lars Schuetze <dev@larsschuetze.de>
2014-06-01 14:25:42 +0200

    relationship editpart does now have label children which are attached dynamically respective bug #55

--- ORM/org.framed.orm.ui/src/org/framed/orm/ui/editPart/connectionkinds/ORMRelationshipEditPart.java
+++ ORM/org.framed.orm.ui/src/org/framed/orm/ui/editPart/connectionkinds/ORMRelationshipEditPart.java
@@ -1,62 +1,62 @@
 package org.framed.orm.ui.editPart.connectionkinds;
 
-import org.eclipse.draw2d.BendpointConnectionRouter;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.draw2d.ConnectionEndpointLocator;
 import org.eclipse.draw2d.IFigure;
-import org.eclipse.draw2d.Label;
+import org.eclipse.draw2d.Locator;
 import org.eclipse.draw2d.PolylineConnection;
+import org.eclipse.gef.EditPart;
 import org.eclipse.swt.SWT;
+import org.framed.orm.model.RelationLabel;
 import org.framed.orm.model.Relationship;
-import org.framed.orm.ui.figure.LabelFigure;
+import org.framed.orm.ui.editPart.ORMRelationLabelEditPart;
 
 /**
- * @author Kay Bierzynski
- * */
+ * @author Kay Bierzynski (initial development)
+ * @author Lars Schuetze (refactoring)
+ **/
 public class ORMRelationshipEditPart extends ORMRelationEditPart {
-	// direct Edit of this labels over Propertysheet
-	private final LabelFigure sourceLabel, targetLabel;
-	
- 
-	
-	public  ORMRelationshipEditPart() {
-	    super();
-	    sourceLabel = new LabelFigure();
-	    targetLabel = new LabelFigure();
-	  }
-	
-	@Override protected IFigure createFigure() {
-		Label tooltipTarget = new Label();
-		tooltipTarget.setText("Direct Editing of this Value over the Propertysheet from the related Realtionship under tragetLabelValue.");
-		Label tooltipSource = new Label();
-		tooltipSource.setText("Direct Editing of this Value over the Propertysheet from the related Realtionship under sourceLabelValue.");
-		
-	    PolylineConnection conn = new PolylineConnection();
-	    conn.setAntialias(SWT.ON);
-        conn.setConnectionRouter(new BendpointConnectionRouter());
-        //add target Label
-        ConnectionEndpointLocator targetEndL = new  ConnectionEndpointLocator(conn,true);
-        targetEndL.setVDistance(-1);
-        targetEndL.setUDistance(1);
-        targetLabel.setText(((Relationship)getModel()).getTargetLabelValue());
-        targetLabel.setToolTip(tooltipTarget);
-        conn.add(targetLabel,targetEndL);
-        //add source Label
-        ConnectionEndpointLocator sourceEndL = new ConnectionEndpointLocator(conn,false);
-        sourceEndL.setVDistance(-1);
-        sourceEndL.setUDistance(1);
-        sourceLabel.setText(((Relationship)getModel()).getSourceLabelValue());
-        sourceLabel.setToolTip(tooltipSource);
-        conn.add(sourceLabel,sourceEndL);
-        
-	    return conn;
-	  }
-	
-	@Override 
-	 protected void refreshVisuals() {
-		super.refreshVisuals();
-		final Relationship model = (Relationship) getModel();
-		
-		sourceLabel.setText(model.getSourceLabelValue());
-		targetLabel.setText(model.getTargetLabelValue());
-	}
+
+  @Override
+  protected IFigure createFigure() {
+    PolylineConnection connection = new PolylineConnection();
+    connection.setAntialias(SWT.ON);
+
+    return connection;
+  }
+
+  private Locator getConnectionLocator(PolylineConnection connection, boolean isEnd) {
+    final ConnectionEndpointLocator locator = new ConnectionEndpointLocator(connection, isEnd);
+    locator.setVDistance(-1);
+    locator.setUDistance(1);
+    return locator;
+  }
+
+  @Override
+  protected List getModelChildren() {
+    List<RelationLabel> children = new ArrayList<>(2);
+    children.add(getRelationship().getTargetLabel());
+    children.add(getRelationship().getSourceLabel());
+    return children;
+  }
+
+  protected Relationship getRelationship() {
+    return (Relationship) getModel();
+  }
+
+  protected PolylineConnection getRelationFigure() {
+    return (PolylineConnection) getFigure();
+  }
+
+  @Override
+  protected void addChildVisual(EditPart childEditPart, int index) {
+    if (childEditPart instanceof ORMRelationLabelEditPart) {
+      ORMRelationLabelEditPart labelEditPart = (ORMRelationLabelEditPart) childEditPart;
+      getRelationFigure().getLayoutManager().setConstraint(labelEditPart.getFigure(),
+          getConnectionLocator(getRelationFigure(), labelEditPart.isRelationEnd()));
+    }
+    super.addChildVisual(childEditPart, index);
+  }
 }
