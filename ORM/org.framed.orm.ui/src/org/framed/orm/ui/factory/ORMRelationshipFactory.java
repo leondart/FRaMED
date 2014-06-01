cd530289c02bb96b572fe38d17d92627ec4999ec
Lars Schuetze <dev@larsschuetze.de>
2014-06-01 14:24:29 +0200

    relationship factory does now generate a relationship without missing labels respective bug #55

--- ORM/org.framed.orm.ui/src/org/framed/orm/ui/factory/ORMRelationshipFactory.java
+++ ORM/org.framed.orm.ui/src/org/framed/orm/ui/factory/ORMRelationshipFactory.java
@@ -1,20 +1,29 @@
 package org.framed.orm.ui.factory;
 
 import org.eclipse.gef.requests.CreationFactory;
-
 import org.framed.orm.model.OrmFactory;
+import org.framed.orm.model.RelationLabel;
 import org.framed.orm.model.Relationship;
 
 /**
  * @author Kay Bierzynski
  * */
-public class ORMRelationshipFactory implements CreationFactory{
-	
-	@Override public Object getNewObject() {
-	    return OrmFactory.eINSTANCE.createRelationship();
-	  }
-	 
-	  @Override public Object getObjectType() {
-	    return Relationship.class;
-	  }
+public class ORMRelationshipFactory implements CreationFactory {
+
+  @Override
+  public Object getNewObject() {
+    Relationship relationship = OrmFactory.eINSTANCE.createRelationship();
+    RelationLabel targetLabel = OrmFactory.eINSTANCE.createRelationLabel();
+    RelationLabel sourceLabel = OrmFactory.eINSTANCE.createRelationLabel();
+    targetLabel.setIsRelationEnd(true);
+    sourceLabel.setIsRelationEnd(false);
+    relationship.setTargetLabel(targetLabel);
+    relationship.setSourceLabel(sourceLabel);
+    return relationship;
+  }
+
+  @Override
+  public Object getObjectType() {
+    return Relationship.class;
+  }
 }
