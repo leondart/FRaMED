7b51a25c6257661976612188c1652773e17fae8b
Kay Bierzynski <s8348434@mail.zih.tu-dresden.de>
2014-07-24 14:32:43 +0200

    added comments to ORMRoleModelCreateCommand and StepCommand, added final to method parametes in both commands, deleted unnecessary code

--- ORM/org.framed.orm.ui/src/org/framed/orm/ui/command/ORMRoleModelCreateCommand.java
+++ ORM/org.framed.orm.ui/src/org/framed/orm/ui/command/ORMRoleModelCreateCommand.java
@@ -2,27 +2,43 @@ package org.framed.orm.ui.command;
 
 
 import org.eclipse.gef.commands.Command;
-
 import org.framed.orm.model.Compartment;
 import org.framed.orm.model.Grouping;
 import org.framed.orm.model.Rolemodel;
 
 /**
+ * Through this command the model element {@link Rolemodel} can be created(invoked into the model tree).
+ * 
  * @author Kay Bierzynski
  * */
 public class ORMRoleModelCreateCommand extends Command {
 
-
+  /**
+   * A {@link Rolemodel} can have as parent a {@link Grouping} or a {@link Compartment}. This
+   * variable contains the parent {@link Grouping} of the {@link Rolemodel}.
+   */
   private Grouping group;
+  /**
+   * A {@link Rolemodel} can have as parent a {@link Grouping} or a {@link Compartment}. This
+   * variable contains the parent {@link Compartment} of the {@link Rolemodel}.
+   */
   private Compartment compartment;
+  /** The variable for the {@link Rolemodel}, which will be created through this command. */
   private Rolemodel rm;
 
+  /**
+   * Constructor of this command, where the label is set, which describes this command to the user,
+   * and initialize the oldFulfilledRoles list.
+   * 
+   * */
   public ORMRoleModelCreateCommand() {
     super.setLabel("ORMRoleModelCreate");
   }
 
   /**
-   * The command can be executed if all parameters have been set.
+   * This method tests if the conditions for executing this command are fulfilled,
+   * 
+   * @return true if the parameter compartment or group is set.
    */
   @Override
   public boolean canExecute() {
@@ -30,33 +46,65 @@ public class ORMRoleModelCreateCommand extends Command {
   }
 
 
-
+  /**
+   * {@inheritDoc} In this method the parent compartment reference of the {@link Rolemodel} is set
+   * when the {@link Compartment} variable was set and the parent grouping reference of the
+   * {@link Rolemodel} is set when the {@link Grouping} variable was set.
+   * 
+   */
   @Override
   public void execute() {
-    if (compartment != null)
+
+    if (compartment != null) {
       rm.setCompartment(compartment);
-    if (group != null)
+    }
+    if (group != null) {
       rm.setParentGroup(group);
+    }
+
   }
 
 
+  /**
+   * {@inheritDoc} To undo this command the parent compartment reference or parent grouping refence
+   * of the {@link Rolemodel} is set on null depended on which parent variable( compartment or
+   * grouping) was set.
+   * 
+   * */
   @Override
   public void undo() {
-    if (compartment != null)
+    if (compartment != null) {
       rm.setCompartment(null);
-    if (group != null)
+    }
+    if (group != null) {
       rm.setParentGroup(null);
+    }
   }
 
-
+  /**
+   * Setter of the {@link Rolemodel}, which should be created/invoked.
+   * The basis {@link Rolemodel} is given by the OrmFactory.eINSTANCE.createRolemodel().
+   * 
+   * @param rm org.framed.orm.model.Rolemodel
+   * */
   public void setRolemodel(final Rolemodel rm) {
     this.rm = rm;
   }
 
+  /**
+   * Setter of the parent {@link Compartment}.
+   * 
+   * @param ct  org.framed.orm.model.Compartment
+   * */
   public void setCompartment(final Compartment ct) {
     this.compartment = ct;
   }
 
+  /**
+   * Setter of the parent {@link Grouping}.
+   * 
+   * @param group  org.framed.orm.model.Grouping
+   * */
   public void setGroup(final Grouping group) {
     this.group = group;
   }
--- ORM/org.framed.orm.ui/src/org/framed/orm/ui/command/StepCommand.java
+++ ORM/org.framed.orm.ui/src/org/framed/orm/ui/command/StepCommand.java
@@ -1,44 +1,94 @@
 package org.framed.orm.ui.command;
 
-import org.eclipse.gef.EditPart;
 import org.eclipse.gef.commands.Command;
 import org.eclipse.gef.editparts.AbstractGraphicalEditPart;
-import org.eclipse.gef.ui.actions.WorkbenchPartAction;
-import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.WorkbenchException;
-import org.eclipse.ui.internal.WorkbenchPartReference;
-import org.eclipse.ui.part.WorkbenchPart;
-import org.framed.orm.ui.editor.ORMGraphicalEditor;
+import org.framed.orm.model.Compartment;
+import org.framed.orm.model.CompartmentDiagram;
+import org.framed.orm.model.Grouping;
+import org.framed.orm.ui.action.StepInAction;
+import org.framed.orm.ui.action.StepInNewPageAction;
+import org.framed.orm.ui.action.StepInNewTabAction;
+import org.framed.orm.ui.action.StepOutAction;
+import org.framed.orm.ui.editPart.ORMGroupingEditPart;
+import org.framed.orm.ui.editPart.types.ORMCompartmentEditPart;
 import org.framed.orm.ui.editor.ORMMultiPageEditor;
 
 /**
+ * This command realize the step in/out, which is initialized by the {@link StepInAction}, the
+ * {@link StepInNewPageAction}, the {@link StepInNewTabAction} or the {@link StepOutAction}, through
+ * changing the content of the viewer of the {@link ORMMultiPageEditor} instance, which owns the
+ * {@link Compartment} or the {@link Grouping} the user wants to step in/out.
  * 
  * @author Kay Bierzynski
  * 
  */
 public class StepCommand extends Command {
 
+  /**
+   * The {@link ORMCompartmentEditPart} or the {@link ORMGroupingEditPart} of the
+   * {@link Compartment} or the {@link Grouping} the user wants to step in/out.
+   */
   private AbstractGraphicalEditPart editpart;
+  /**
+   * The {@link ORMMultiPageEditor} instance, which owns the {@link Compartment} or the
+   * {@link Grouping} you want to step in/out and whose viewer content this command changes.
+   */
   private ORMMultiPageEditor editorPart;
+  /**
+   * This object variable represents a {@link Compartment} or a {@link Grouping} model element, when
+   * you want to step in, and represents a {@link Compartment}, the {@link CompartmentDiagram} or a
+   * {@link Grouping} model element, when you want to step out. All in all holds this variable the
+   * new content for the viewer of the active {@link ORMMultiPageEditor} instance.
+   */
   private Object newContent;
+  /**
+   * A flag that indicates that the user wants to step in a new window. This variable is true when
+   * this command is called/requested by {@link StepInNewPageAction}.
+   */
   private boolean isNewWindowCommand;
+  /**
+   * A flag that indicates that the user wants to step in a new tab. This variable is true when this
+   * command is called/requested by {@link StepInNewTabAction}.
+   */
   private boolean isNewTabCommand;
 
+  /**
+   * This method tests if the conditions for executing this command are fulfilled,
+   * 
+   * @return true if the parameters editpart, editorPart and newContent is set.
+   */
   @Override
   public boolean canExecute() {
 
     return editpart != null && editorPart != null && newContent != null;
   }
 
+  /**
+   * {@inheritDoc} The first thing that is done in this method is that a new editor page is opened
+   * when this command is called/requested by {@link StepInNewPageAction} or that the method gets
+   * the active editor page in any other case. After that a new {@link ORMMultiPageEditor} instance
+   * is created, the initial viewer content of the new {@link ORMMultiPageEditor} instance is
+   * replaced with the newContent, a custom title based on the newContent which is shown in the tab
+   * is created for the new editor instance and the editortype of the childeditors is updated to
+   * adjust what is accessible in the editor pallet. All this is done in the case that this command
+   * was called/requested by the {@link StepInNewPageAction} or the {@link StepInNewTabAction} the
+   * initial viewer content. In any other case all thing which where listed for a new
+   * {@link ORMMultiPageEditor} instance are done for the active {@link ORMMultiPageEditor}
+   * instance.
+   * 
+   */
   @Override
   public void execute() {
 
+    // the page, which in the user wants to open a new tab or step into as a complete new page
     IWorkbenchPage page = null;
 
     if (isNewWindowCommand) {
 
+      // try to open a new editor page
       try {
         page = editorPart.getSite().getWorkbenchWindow().openPage(editorPart.getSite());
       } catch (WorkbenchException e) {
@@ -47,24 +97,22 @@ public class StepCommand extends Command {
       }
 
     } else {
+      // get the active editor page
       page = editorPart.getSite().getWorkbenchWindow().getActivePage();
     }
 
-    IEditorInput input = editorPart.getEditorInput();
-
-
     if (isNewWindowCommand || isNewTabCommand) {
       try {
-        // open new editor instance with old content edipart model
+        // try to open a new ORMMultiPageEditor instance
         ORMMultiPageEditor newPart =
-            (ORMMultiPageEditor) page.openEditor(input, "ORMEditor.editorID", false,
-                IWorkbenchPage.MATCH_NONE);
+            (ORMMultiPageEditor) page.openEditor(editorPart.getEditorInput(), "ORMEditor.editorID",
+                false, IWorkbenchPage.MATCH_NONE);
 
-
+        // try to change the viewer content of the new instance
         newPart.setContents(newContent);
+        // try to create a new title for the new instance based on the newContent
         newPart.createCustomTitleForEditor(newContent);
-
-
+        // try to update the editor type of the childeditors of the new instance
         newPart.getBehaviorEditor().updateEditorType();
 
       } catch (PartInitException e1) {
@@ -72,53 +120,71 @@ public class StepCommand extends Command {
         e1.printStackTrace();
       }
     } else {
+      // change the content of the viewer of the active ORMMultiPageEditor instance
       editorPart.setContents(newContent);
+      // create a new title for the active instance based on the newContent
       editorPart.createCustomTitleForEditor(newContent);
-      editorPart.setFocus();
-      editorPart.getBehaviorEditor().setFocus();
-      editorPart.setActiveEditor(editorPart);
-//      editorPart.getSite().getWorkbenchWindow().
-//      editorPart.getBehaviorEditor().getOwnViewer().getContents().deactivate();
-//      editorPart.getBehaviorEditor().getOwnViewer().getContents().activate();
-//      editorPart.getBehaviorEditor().getOwnViewer().getContents().setFocus(true);
-//      editorPart.getBehaviorEditor().getOwnViewer().getContents().setSelected(0);
-          //      System.out.println(editorPart.getSelectedPage());
-//      editorPart.getSite().getWorkbenchWindow().setActivePage((IWorkbenchPage) ((ORMGraphicalEditor)editorPart.getSelectedPage()).getSite().getPage());
-//      System.out.println("Focus set");
-//      editpart.setSelected(1);
-//      editpart.setFocus(true);
-//      editpart.setSelected(0);
+      // update the editor type of the childeditors of the active instance
       editorPart.getBehaviorEditor().updateEditorType();
     }
   }
 
   /**
-   * This command can for now not be undone.
+   * Method, which determines if the the command can be undone or not.
+   * 
+   * @return false this command can never be undone, because the change of the viewer should always
+   *         happen through step actions also through context menu or shortcut
    */
   @Override
   public boolean canUndo() {
     return false;
   }
 
-  public void setEditPart(AbstractGraphicalEditPart editpart) {
-//    System.out.println("EditPart: " + editpart);
+  /**
+   * Setter for the editpart, whose model is the new viewer content.
+   * 
+   * @param editpart org.eclipse.gef.editparts.AbstractGraphicalEditPart
+   * */
+  public void setEditPart(final AbstractGraphicalEditPart editpart) {
     this.editpart = editpart;
   }
 
-  public void setEditorPart(ORMMultiPageEditor editorPart) {
+  /**
+   * Setter for the editorpart (active ORMMultiPageEditor instance).
+   * 
+   * @param editorPart org.framed.orm.ui.editor.ORMMultiPageEditor
+   */
+  public void setEditorPart(final ORMMultiPageEditor editorPart) {
     this.editorPart = editorPart;
   }
 
-  public void setNewContent(Object content) {
+  /**
+   * Setter for the new viewer content.
+   * 
+   * @param content java.lang.Object
+   */
+  public void setNewContent(final Object content) {
     this.newContent = content;
   }
 
-  public void setIsNewWindowCommand(boolean isNewWindowCommand) {
+  /**
+   * Setter for the flage, which indicates if this command is called/requested by
+   * {@link StepInNewPageAction} or not.
+   * 
+   * @param isNewWindowCommand boolean
+   */
+  public void setIsNewWindowCommand(final boolean isNewWindowCommand) {
     this.isNewWindowCommand = isNewWindowCommand;
   }
 
-  public void setIsNewTabCommand(boolean isNewTabCommand){
+  /**
+   * Setter for the flage, which indicates if this command is called/requested by
+   * {@link StepInNewTabAction} or not.
+   * 
+   * @param isNewTabCommand boolean
+   */
+  public void setIsNewTabCommand(final boolean isNewTabCommand) {
     this.isNewTabCommand = isNewTabCommand;
   }
-  
+
 }
