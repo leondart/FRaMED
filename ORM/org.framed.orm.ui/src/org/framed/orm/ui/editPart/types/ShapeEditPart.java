65339b3ee7041411da3a2a8c881d77f00f073381
bgyapjas <balint.gyapjas@tu-dresden.de>
2014-12-02 21:04:37 +0100

    New class ShapeEditPart created. methods added: createFigure(); getConstraints()

---
+++ ORM/org.framed.orm.ui/src/org/framed/orm/ui/editPart/types/ShapeEditPart.java
@@ -0,0 +1,38 @@
+package org.framed.orm.ui.editPart.types;
+
+import org.eclipse.draw2d.IFigure;
+import org.eclipse.draw2d.geometry.Point;
+import org.eclipse.draw2d.geometry.Rectangle;
+import org.eclipse.gef.editparts.AbstractGraphicalEditPart;
+import org.framed.orm.model.Shape;
+import org.framed.orm.ui.figure.ORMFigureFactory;
+
+/**
+ * 
+ * @author Bálint Gyapjas
+ *
+ */
+public class ShapeEditPart extends AbstractGraphicalEditPart {
+
+	@Override
+	protected IFigure createFigure() {
+		return ORMFigureFactory.createFigure(this);
+	}
+
+	@Override
+	protected void createEditPolicies() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public Rectangle getConstraints() {
+		Shape model = (Shape) getModel();
+		org.framed.orm.geometry.Rectangle boundries = model.getBoundaries();
+		
+		Point	topLeft = new Point( boundries.getTopLeft().getX(), boundries.getTopLeft().getX()),
+				bottomRight = new Point( boundries.getBottomRight().getX(), boundries.getBottomRight().getY());
+		
+		return new org.eclipse.draw2d.geometry.Rectangle(topLeft,bottomRight);		
+	}
+
+}
