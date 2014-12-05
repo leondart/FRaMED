4cf3b08bee5755000d854da6a9fe161c20efd4a9
leondart <s8348434@mail.zih.tu-dresden.de>
2014-12-05 21:40:12 +0100

    genrated test skeleton for RelationshipConstraintsAction, ORMRelationshipConstraintDeleteCommand and ORMRelationCreateCommand

---
+++ ORM/org.framed.orm.ui.tests/src/org/framed/orm/ui/tests/action/RelationshipConstraintsActionTest.java
@@ -0,0 +1,14 @@
+package org.framed.orm.ui.tests.action;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RelationshipConstraintsActionTest {
+
+  @Test
+  public void testCalculateEnabled() {
+    fail("Not yet implemented");
+  }
+
+}
---
+++ ORM/org.framed.orm.ui.tests/src/org/framed/orm/ui/tests/command/connectionkinds/ORMRelationCreateCommandTest.java
@@ -0,0 +1,24 @@
+package org.framed.orm.ui.tests.command.connectionkinds;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ORMRelationCreateCommandTest {
+
+  @Test
+  public void testCanExecute() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testExecute() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testUndo() {
+    fail("Not yet implemented");
+  }
+
+}
