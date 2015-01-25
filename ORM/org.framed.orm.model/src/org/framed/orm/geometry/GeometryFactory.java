a943e65ce627b8e518976ac18d7d3d84b517b523
Kay Bierzynski <s8348434@mail.zih.tu-dresden.de>
2015-01-25 17:21:11 +0100

    added Type RelationshipShapeChild for the implementation of RelationshipImplication

--- ORM/org.framed.orm.model/src/org/framed/orm/geometry/GeometryFactory.java
+++ ORM/org.framed.orm.model/src/org/framed/orm/geometry/GeometryFactory.java
@@ -13,49 +13,48 @@ import org.eclipse.emf.ecore.EFactory;
  * @generated
  */
 public interface GeometryFactory extends EFactory {
-	/**
-	 * The singleton instance of the factory.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	GeometryFactory eINSTANCE = org.framed.orm.geometry.impl.GeometryFactoryImpl
-			.init();
+  /**
+   * The singleton instance of the factory.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  GeometryFactory eINSTANCE = org.framed.orm.geometry.impl.GeometryFactoryImpl.init();
 
-	/**
-	 * Returns a new object of class '<em>Point</em>'.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @return a new object of class '<em>Point</em>'.
-	 * @generated
-	 */
-	Point createPoint();
+  /**
+   * Returns a new object of class '<em>Point</em>'.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return a new object of class '<em>Point</em>'.
+   * @generated
+   */
+  Point createPoint();
 
-	/**
-	 * Returns a new object of class '<em>Rectangle</em>'.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @return a new object of class '<em>Rectangle</em>'.
-	 * @generated
-	 */
-	Rectangle createRectangle();
+  /**
+   * Returns a new object of class '<em>Rectangle</em>'.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return a new object of class '<em>Rectangle</em>'.
+   * @generated
+   */
+  Rectangle createRectangle();
 
-	/**
-	 * Returns a new object of class '<em>Relative Point</em>'.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @return a new object of class '<em>Relative Point</em>'.
-	 * @generated
-	 */
-	RelativePoint createRelativePoint();
+  /**
+   * Returns a new object of class '<em>Relative Point</em>'.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return a new object of class '<em>Relative Point</em>'.
+   * @generated
+   */
+  RelativePoint createRelativePoint();
 
-	/**
-	 * Returns the package supported by this factory.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @return the package supported by this factory.
-	 * @generated
-	 */
-	GeometryPackage getGeometryPackage();
+  /**
+   * Returns the package supported by this factory.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return the package supported by this factory.
+   * @generated
+   */
+  GeometryPackage getGeometryPackage();
 
 } //GeometryFactory
--- ORM/org.framed.orm.model/src/org/framed/orm/model/impl/RelationImpl.java
+++ ORM/org.framed.orm.model/src/org/framed/orm/model/impl/RelationImpl.java
@@ -34,130 +34,125 @@ import org.framed.orm.model.Shape;
  * @generated
  */
 public class RelationImpl extends ModelElementImpl implements Relation {
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	protected RelationImpl() {
-		super();
-	}
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  protected RelationImpl() {
+    super();
+  }
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	@Override
-	protected EClass eStaticClass() {
-		return OrmPackage.Literals.RELATION;
-	}
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  protected EClass eStaticClass() {
+    return OrmPackage.Literals.RELATION;
+  }
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	public ModelElement getTarget() {
-		return (ModelElement) eGet(OrmPackage.Literals.RELATION__TARGET, true);
-	}
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public ModelElement getTarget() {
+    return (ModelElement) eGet(OrmPackage.Literals.RELATION__TARGET, true);
+  }
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	public void setTarget(ModelElement newTarget) {
-		eSet(OrmPackage.Literals.RELATION__TARGET, newTarget);
-	}
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public void setTarget(ModelElement newTarget) {
+    eSet(OrmPackage.Literals.RELATION__TARGET, newTarget);
+  }
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	public ModelElement getSource() {
-		return (ModelElement) eGet(OrmPackage.Literals.RELATION__SOURCE, true);
-	}
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public ModelElement getSource() {
+    return (ModelElement) eGet(OrmPackage.Literals.RELATION__SOURCE, true);
+  }
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	public void setSource(ModelElement newSource) {
-		eSet(OrmPackage.Literals.RELATION__SOURCE, newSource);
-	}
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public void setSource(ModelElement newSource) {
+    eSet(OrmPackage.Literals.RELATION__SOURCE, newSource);
+  }
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	@SuppressWarnings("unchecked")
-	public EList<RelativePoint> getBendpoints() {
-		return (EList<RelativePoint>) eGet(
-				OrmPackage.Literals.RELATION__BENDPOINTS, true);
-	}
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @SuppressWarnings("unchecked")
+  public EList<RelativePoint> getBendpoints() {
+    return (EList<RelativePoint>) eGet(OrmPackage.Literals.RELATION__BENDPOINTS, true);
+  }
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	public NamedElement getSourceLabel() {
-		return (NamedElement) eGet(OrmPackage.Literals.RELATION__SOURCE_LABEL,
-				true);
-	}
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public NamedElement getSourceLabel() {
+    return (NamedElement) eGet(OrmPackage.Literals.RELATION__SOURCE_LABEL, true);
+  }
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	public void setSourceLabel(NamedElement newSourceLabel) {
-		eSet(OrmPackage.Literals.RELATION__SOURCE_LABEL, newSourceLabel);
-	}
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public void setSourceLabel(NamedElement newSourceLabel) {
+    eSet(OrmPackage.Literals.RELATION__SOURCE_LABEL, newSourceLabel);
+  }
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	public NamedElement getTargetLabel() {
-		return (NamedElement) eGet(OrmPackage.Literals.RELATION__TARGET_LABEL,
-				true);
-	}
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public NamedElement getTargetLabel() {
+    return (NamedElement) eGet(OrmPackage.Literals.RELATION__TARGET_LABEL, true);
+  }
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	public void setTargetLabel(NamedElement newTargetLabel) {
-		eSet(OrmPackage.Literals.RELATION__TARGET_LABEL, newTargetLabel);
-	}
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public void setTargetLabel(NamedElement newTargetLabel) {
+    eSet(OrmPackage.Literals.RELATION__TARGET_LABEL, newTargetLabel);
+  }
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	@SuppressWarnings("unchecked")
-	public EList<Relation> getReferencedRelation() {
-		return (EList<Relation>) eGet(
-				OrmPackage.Literals.RELATION__REFERENCED_RELATION, true);
-	}
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @SuppressWarnings("unchecked")
+  public EList<Relation> getReferencedRelation() {
+    return (EList<Relation>) eGet(OrmPackage.Literals.RELATION__REFERENCED_RELATION, true);
+  }
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	@SuppressWarnings("unchecked")
-	public EList<Shape> getReferencedRoles() {
-		return (EList<Shape>) eGet(
-				OrmPackage.Literals.RELATION__REFERENCED_ROLES, true);
-	}
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @SuppressWarnings("unchecked")
+  public EList<Shape> getReferencedRoles() {
+    return (EList<Shape>) eGet(OrmPackage.Literals.RELATION__REFERENCED_ROLES, true);
+  }
 
 } //RelationImpl
