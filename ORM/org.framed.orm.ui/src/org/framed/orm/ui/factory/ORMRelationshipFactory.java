928b67e6301cd26fba59a854c0c0c71ccdcb5896
Kay Bierzynski <s8348434@mail.zih.tu-dresden.de>
2015-01-25 17:36:21 +0100

    Relationships are now created with RelationshipShapeChild

--- ORM/org.framed.orm.ui/src/org/framed/orm/ui/factory/ORMRelationshipFactory.java
+++ ORM/org.framed.orm.ui/src/org/framed/orm/ui/factory/ORMRelationshipFactory.java
@@ -1,8 +1,12 @@
 package org.framed.orm.ui.factory;
 
 import org.eclipse.gef.requests.CreationFactory;
+import org.framed.orm.geometry.GeometryFactory;
+import org.framed.orm.geometry.Point;
+import org.framed.orm.geometry.Rectangle;
 import org.framed.orm.model.OrmFactory;
 import org.framed.orm.model.Relation;
+import org.framed.orm.model.Shape;
 import org.framed.orm.model.Type;
 
 /**
@@ -16,6 +20,7 @@ public class ORMRelationshipFactory implements CreationFactory {
   public Object getNewObject() {
     Relation relation = OrmFactory.eINSTANCE.createRelation();
     relation.setName(null);
+    relation.getReferencedRoles().add(createRelationshipShapeChild());
     relation.setType(Type.RELATIONSHIP);
     return relation;
   }
@@ -23,5 +28,33 @@ public class ORMRelationshipFactory implements CreationFactory {
   @Override
   public Object getObjectType() {
     return Type.RELATIONSHIP;
+  }
+  
+  private Shape createRelationshipShapeChild(){
+    Shape shape = OrmFactory.eINSTANCE.createShape();
+    shape.setContainer(null);
+    shape.setType(Type.RELATIONSHIP_SHAPE_CHILD);
+    shape.setName("");
+    shape.setModel(null);
+    shape.setFirstSegment(null);
+    shape.setSecondSegment(null);
+    shape.setDescription(null);
+    
+    Rectangle rec = GeometryFactory.eINSTANCE.createRectangle();
+
+    Point bottomRight = GeometryFactory.eINSTANCE.createPoint();
+    Point topLeft = GeometryFactory.eINSTANCE.createPoint();
+
+    bottomRight.setX(1);
+    bottomRight.setY(0);
+    topLeft.setX(0);
+    topLeft.setY(1);
+
+    rec.setBottomRight(bottomRight);
+    rec.setTopLeft(topLeft);
+    
+    shape.setBoundaries(rec);
+    
+    return shape;
   }
 }
