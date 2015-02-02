8d39bc71b0dd82573e37acffdc43c1323c79b3c3
Johannes Tandler <jakefoly@gmail.com>
2015-02-02 12:22:35 +0100

    fix OrmExample.java
    Signed-off-by: Johannes Tandler <jakefoly@gmail.com>

--- ORM/org.framed.orm.model.tests/src/org/framed/orm/model/tests/OrmExample.java
+++ ORM/org.framed.orm.model.tests/src/org/framed/orm/model/tests/OrmExample.java
@@ -49,7 +49,7 @@ public class OrmExample {
     if (args.length == 0) {
       System.out.println("Enter a list of file paths or URIs that have content like this:");
       try {
-        Resource resource = resourceSet.createResource(URI.createURI("http:///My.orm"));
+        Resource resource = resourceSet.createResource(URI.createURI("http:///My.graphical.crom"));
         NamedElement root = OrmFactory.eINSTANCE.createNamedElement();
         resource.getContents().add(root);
         resource.save(System.out, null);
